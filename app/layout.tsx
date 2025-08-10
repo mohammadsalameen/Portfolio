98bf4c2a47d055bd3a8709db6bf52af6d021cd8c
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-08-10 10:02:52 +0400

    Add a Responsive Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ResponsiveNavbar from "@/components/Home/Navbar/ResponsiveNavbar";
+import Footer from "@/components/Footer/Footer";
 
 const font = Inter({
   weight : ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,6 +26,7 @@ export default function RootLayout({
       >
         <ResponsiveNavbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
