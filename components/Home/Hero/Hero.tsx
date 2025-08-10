969e57b854ecb7a50c2f5c1c7a9a9eb8f2adc068
Mohammad Salameen <anas-20111-2011@hotmail.com>
2025-08-11 00:21:43 +0400

    Edit Hero section

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Typewriter from "typewriter-effect";
 import { BsArrowRight } from "react-icons/bs";
 import ParticlesHero from "./ParticleBackground";
+import { ContactData } from "@/constant/constant";
 const Hero = () => {
   return (
     <div className="relative  h-screen flex items-center justify-center text-white overflow-hidden flex-col">
@@ -51,10 +52,10 @@ const Hero = () => {
             />
           </span>
         </h2>
-        <button data-aos= 'fade-up' data-aos-delay='600' className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium">
+        <a href={ContactData.github} target="_blank" data-aos= 'fade-up' data-aos-delay='600' className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium">
           <span>See my work</span>
           <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
-        </button>
+        </a>
       </div>
     </div>
   );
